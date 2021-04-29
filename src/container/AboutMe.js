5a15afc79b9b93efdac4fa194d86fbeaee606e32
erlangga-da <erlanggamuhammad94@gmail.com>
2021-04-29 10:03:48 +0700

    header cv
    model baru header cv

--- src/container/AboutMe.js
+++ src/container/AboutMe.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import profile from"../img/pp.jpg"
+import "../css/pattern.min.css";
 import "../css/index.css";
 
 export default class AboutMe extends Component {
@@ -9,18 +9,22 @@ export default class AboutMe extends Component {
       <div>
         <Container>
           <Row>
-            <Col sm={12} md={5}>
-              <div id="img-container">
-                <div id="img"></div>
+            <Col sm={12} md={10}>
+              <div id="text-container">
+                <h1>muhammad</h1>
+                <h1>erlangga</h1>
+                <p>student</p>
               </div>
             </Col>
-            <Col sm={12} md={7}>
-              <div id="text-container">
-                <h2>Muhammad</h2>
-                <h1>Erlangga</h1>
+            <Col sm={12} md={2}>
+              <div id="img-container">
+                <div id="img-wrapper" className="pattern-dots-md gray-light">
+                  <div id="img"></div>
+                </div>
               </div>
             </Col>
           </Row>
+          <hr id="divider" />
         </Container>
       </div>
     );
